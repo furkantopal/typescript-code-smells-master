a421a1ba2023e66795d24f5eb0df0d7fe8261a6e
furkantopal <topallfurkan@gmail.com>
2022-02-26 15:41:42 +0100

    Squashed commit of the following:
    commit db27f0a794dcf365ff138a1152a78922381a030f
    Author: furkantopal <topallfurkan@gmail.com>
    Date:   Sat Feb 26 15:40:39 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 1bb700d5d73515595b6c074f0b6a642aaa656623
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 19:22:38 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit babcae6ed8f1933076f76569a3cb20d3226dfba9
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 18:53:04 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit fd626ea2b7c869b3c7feb7de6bea7dbdea25d036
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 17:52:42 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 02b7eb8a8f9e227123e63effb4cbd1eb374847fe
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 17:45:15 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit a5b60a9a4c7aac456bfadaf20518f9feba11f8c9
    Author: Simon Reekie <simon.reekie@cazoo.co.uk>
    Date:   Fri Feb 25 16:50:55 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 110dde44578886437d9ef22d9b5b0d6e38babe1e
    Author: Simon Reekie <simon.reekie@cazoo.co.uk>
    Date:   Fri Feb 25 16:48:54 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit bc41c4a6c173bb4a997202005a8f0289c535a6d1
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 16:44:54 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 15da4076d858aff00b907ffeee605ae116e0a1fb
    Author: furkantopal <topallfurkan@gmail.com>
    Date:   Fri Feb 25 17:36:19 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 22648e33493ad7ee5e053776fe0c5552bca5929c
    Author: Simon Reekie <simon.reekie@cazoo.co.uk>
    Date:   Fri Feb 25 16:20:32 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 56cd7968661ed7df8833fe93e190a24cee2cbbbd
    Author: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Date:   Fri Feb 25 16:07:50 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit ab067a6df6edf04ed3dcc9cff748ff7cd82bfdd5
    Author: furkantopal <topallfurkan@gmail.com>
    Date:   Fri Feb 25 16:55:21 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: Vikram Nadig <vikram.nadig@cazoo.co.uk>
    Co-authored-by: Simon Reekie <simon.reekie@cazoo.co.uk>

--- src/Game.ts
+++ src/Game.ts
@@ -1,103 +1,100 @@
+export enum Sign {
+  O = 'O',
+  X = 'X',
+  notPlayed = ' ',
+}
+
+export interface Coordinate {
+  x: number;
+  y: number;
+}
+
 export class Game {
-    private _lastSymbol: string = ' ';
-    private _board: Board = new Board();
+  private _lastSymbol: Sign = Sign.notPlayed;
+  private _board: Board = new Board();
 
-    public Play(symbol: string, x: number, y: number) : void {
-        //if first move
-        if (this._lastSymbol == ' ') {
-            //if player is X
-            if (symbol == 'O') {
-                throw new Error("Invalid first player");
-            }
-        }
-        //if not first move but player repeated
-        else if (symbol == this._lastSymbol) {
-            throw new Error("Invalid next player");
-        }
-        //if not first move but play on an already played tile
-        else if (this._board.TileAt(x, y).Symbol != ' ') {
-            throw new Error("Invalid position");
-        }
-
-        // update game state
-        this._lastSymbol = symbol;
-        this._board.AddTileAt(symbol, x, y);
+  public Play(symbol: Sign, coordinate: Coordinate): void {
+    if (this.isFirstMove() && this.isPlayerX(symbol)) {
+      throw new Error('Invalid first player');
     }
-
-    public Winner() : string {
-        //if the positions in first row are taken
-        if (this._board.TileAt(0, 0)!.Symbol != ' ' &&
-                this._board.TileAt(0, 1)!.Symbol != ' ' &&
-                this._board.TileAt(0, 2)!.Symbol != ' ') {
-            //if first row is full with same symbol
-            if (this._board.TileAt(0, 0)!.Symbol ==
-                    this._board.TileAt(0, 1)!.Symbol &&
-                    this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol) {
-                return this._board.TileAt(0, 0)!.Symbol;
-            }
-        }
-
-        //if the positions in first row are taken
-        if (this._board.TileAt(1, 0)!.Symbol != ' ' &&
-                this._board.TileAt(1, 1)!.Symbol != ' ' &&
-                this._board.TileAt(1, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(1, 0)!.Symbol ==
-                    this._board.TileAt(1, 1)!.Symbol &&
-                    this._board.TileAt(1, 2)!.Symbol ==
-                            this._board.TileAt(1, 1)!.Symbol) {
-                return this._board.TileAt(1, 0)!.Symbol;
-            }
-        }
-
-        //if the positions in first row are taken
-        if (this._board.TileAt(2, 0)!.Symbol != ' ' &&
-                this._board.TileAt(2, 1)!.Symbol != ' ' &&
-                this._board.TileAt(2, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(2, 0)!.Symbol ==
-                    this._board.TileAt(2, 1)!.Symbol &&
-                    this._board.TileAt(2, 2)!.Symbol ==
-                            this._board.TileAt(2, 1)!.Symbol) {
-                return this._board.TileAt(2, 0)!.Symbol;
-            }
-        }
-
-        return ' ';
+    if (symbol == this._lastSymbol) {
+      throw new Error('Invalid next player');
     }
+    this._board.MarkTile({Symbol:symbol,Coordinate:coordinate});
+    this._lastSymbol = symbol;
+  }
+
+  private isPlayerX = (symbol: Sign) => symbol == Sign.O;
+
+  private isFirstMove = () => this._lastSymbol == Sign.notPlayed;
+
+  public Winner(): Sign {
+    const boardState = this._board.getBoardState();
+    if (boardState.Status !== 'Complete') {
+      return Sign.notPlayed;
+    }
+    return boardState.CurrentState
+  }
+
 }
 
-interface Tile
-{
-    X: number;
-    Y: number;
-    Symbol: string;
+interface Tile {
+  Coordinate:Coordinate
+  Symbol: Sign;
 }
 
-class Board
-{
-    private _plays : Tile[] = [];
-
-    constructor()
-    {
-        for (let i = 0; i < 3; i++)
-        {
-            for (let j = 0; j < 3; j++)
-            {
-                const tile : Tile = {X :i, Y:j, Symbol:" "};
-                this._plays.push(tile);
-            }
-        }
-    }
-
-    public TileAt(x:  number, y: number): Tile {
-        return this._plays.find((t:Tile) => t.X == x && t.Y == y)!
-    }
-
-    public AddTileAt(symbol: string, x: number, y: number) : void
-    {
-        const tile : Tile = {X :x, Y:y, Symbol:symbol};
-
-        this._plays.find((t:Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
-    }
+interface BoardState {
+  Status:'Complete'| 'InPlay'
+  CurrentState: Sign
 }
+
+class Board {
+  private _plays: Tile[] = [];
+
+  constructor() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const tile: Tile = { Coordinate: {x: i, y: j}, Symbol: Sign.notPlayed };
+        this._plays.push(tile);
+      }
+    }
+  }
+
+  public getBoardState(): BoardState {
+    for (let i = 0; i < 3; i++) {
+      if (this.isFirstRowComplete(i) && this.isMiddleRowComplete(i)) {
+        return {Status: 'Complete', CurrentState: this.SymbolAt({x: i, y: 0})!};
+      }
+    }
+    return {Status:'InPlay',CurrentState:Sign.notPlayed}
+
+  }
+  private isFirstRowComplete(tileX: number): boolean {
+    return (
+        this.SymbolAt({x: tileX, y: 0})! != Sign.notPlayed &&
+        this.SymbolAt({x: tileX, y: 1})! != Sign.notPlayed &&
+        this.SymbolAt({x: tileX, y: 2})! != Sign.notPlayed
+    );
+  }
+
+  private isBoardPositionPlayed = (coordinate: Coordinate): boolean => this.SymbolAt(coordinate) != Sign.notPlayed;
+
+  private SymbolAt(coordinate: Coordinate): Sign {
+    return this._plays.find((t: Tile) => t.Coordinate.x == coordinate.x && t.Coordinate.y == coordinate.y)!.Symbol;
+  }
+
+  public MarkTile(tile: Tile): void {
+    if (this.isBoardPositionPlayed(tile.Coordinate)) {
+      throw new Error('Invalid position');
+    }
+    this._plays.find((t: Tile) => t.Coordinate.x == tile.Coordinate.x && t.Coordinate.y == tile.Coordinate.y)!.Symbol = tile.Symbol;
+  }
+
+  private isMiddleRowComplete(tileX: number):boolean {
+    return (
+        this.SymbolAt({x: tileX, y: 0})! == this.SymbolAt({x: tileX, y: 1})! &&
+        this.SymbolAt({x: tileX, y: 2})! == this.SymbolAt({x: tileX, y: 1})!
+    );
+  }
+}
+//test
